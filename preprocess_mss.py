f70ef71f755f35e143c055f8bc6a9d4275c47060
Erick Mas <mas@irides.tohoku.ac.jp>
2024-07-09 11:00:38 +0900

    AnDeS script

---
+++ preprocess_mss.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from datetime import datetime, timedelta
+
+import os
+import pandas as pd
+import numpy as np
+import time
+
+Pyear = 2024
+
+def save_data():
+    t0 = time.time()
+    data = {}
+    from_date = datetime(year=Pyear, month=1, day=1)
+    # to_date = datetime(year=Pyear+1, month=1, day=1)
+    to_date = datetime(year=Pyear, month=7, day=1)
+    num_rows = (to_date - from_date) // timedelta(hours=1)
+    cur = from_date
+    i = 0
+    while cur < to_date:
+        path = cur.strftime("/Volumes/Pegasus32/data/NTT_Data/%Y_csv/%Y%m%d/clipped_mesh_pop_%Y%m%d%H00_00000.csv")
+        print(cur)
+        
+        if not os.path.isfile(path):
+            print('passed')
+            continue
+
+        df = pd.read_csv(path)
+        for row in df.itertuples(index=False):
+            if row.area not in data:
+                data[row.area] = np.full(num_rows, -1, dtype=np.int32)
+            data[row.area][i] = row.population
+
+        print(path, len(data))
+
+        cur += timedelta(hours=1)
+        i += 1
+
+    out = np.lib.format.open_memmap(f"./data/ntt_mss_{Pyear}.npy", mode="w+", dtype=np.int32,
+                                    shape=(num_rows, len(data)), fortran_order=True)
+
+    for i, v in enumerate(data.values()):
+        out[:, i] = v
+
+    out.flush()
+
+    np.save(f"./data/ntt_mss_{Pyear}_areas.npy", np.fromiter(data.keys(), dtype=np.int32))
+
+    print(f'{time.time() - t0}')
+    return
+
+
+if __name__ == "__main__":
+    save_data()
